fd62dbae47d27cd0d2568f1f20244e1370a99b2d
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-17 00:09:41 +0100

    some modifications, to code

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,43 +1,23 @@
-/* eslint-disable import/no-named-as-default */
 import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
-/**
- * Controller class for handling application endpoints related to status and statistics.
- */
-export default class AppController {
-  /**
-   * Handler for GET /status endpoint.
-   * Checks the status of Redis and MongoDB.
-   * @param {import('express').Request} req - The Express request object.
-   * @param {import('express').Response} res - The Express response object.
-   */
-  static getStatus(req, res) {
-    try {
-      const redisAlive = redisClient.isAlive();
-      const dbAlive = dbClient.isAlive();
 
-      res.status(200).json({ redis: redisAlive, db: dbAlive });
-    } catch (error) {
-      console.error('Error checking status:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
+class AppController {
+  async getStatus(req, res) {
+    const data = {
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    };
+    res.status(200).json(data);
   }
 
-  /**
-   * Handler for GET /stats endpoint.
-   * Retrieves the number of users and files from the database.
-   * @param {import('express').Request} req - The Express request object.
-   * @param {import('express').Response} res - The Express response object.
-   */
-  static getStats(req, res) {
-    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
-      .then(([usersCount, filesCount]) => {
-        res.status(200).json({ users: usersCount, files: filesCount });
-      })
-      .catch((error) => {
-        console.error('Error fetching stats:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-      });
+  async getStats(req, res) {
+    const data = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).json(data);
   }
-}
+};
+
+module.exports = new AppController();
