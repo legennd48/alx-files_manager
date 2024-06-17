ae08e4e44a75a484167b7f1d8b712e880eb10c29
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-17 13:18:14 +0100

    reverted changes

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,23 +2,18 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  constructor() {
-    this.redisClient = redisClient;
-    this.dbClient = dbClient;
-  }
-
   async getStatus(req, res) {
     const data = {
-      redis: await this.redisClient.isAlive(),
-      db: await this.dbClient.isAlive(),
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
     };
     res.status(200).json(data);
   }
 
   async getStats(req, res) {
     const data = {
-      users: await this.dbClient.nbUsers(),
-      files: await this.dbClient.nbFiles(),
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
     };
     res.status(200).json(data);
   }
