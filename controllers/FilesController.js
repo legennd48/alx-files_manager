08266b5a789e610907a43770ba610546efcc3a7a
Adewale Liasu <132359431+legennd48@users.noreply.github.com>
2024-06-19 13:12:07 +0100

    small mod to FilesController

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -88,7 +88,7 @@ class FilesController {
     }
 
     const fileId = req.params.id;
-    const file = await dbClient.db.collection('files').findOne({ _id: new dbClient.ObjectID(fileId), userId: new dbClient.ObjectID(userId) });
+    const file = await dbClient.db.collection('files').findOne({ _id: new dbClient.ObjectID(fileId) });
 
     if (!file) {
       return res.status(404).json({ error: 'Not found' });
@@ -113,11 +113,18 @@ class FilesController {
     const page = parseInt(req.query.page, 10) || 0;
     const pageSize = 20;
 
-    const files = await dbClient.db.collection('files')
-      .find({ userId: new dbClient.ObjectID(userId), parentId })
-      .skip(page * pageSize)
-      .limit(pageSize)
-      .toArray();
+    if (parentId !== 0) {
+      const parentFile = await dbClient.db.collection('files').findOne({ _id: parentId });
+      if (!parentFile) {
+        return res.status(400).json({ error: 'Parent not found' });
+      }
+    }
+
+    const files = await dbClient.db.collection('files').aggregate([
+      { $match: { userId: new dbClient.ObjectID(userId), parentId } },
+      { $skip: page * pageSize },
+      { $limit: pageSize },
+    ]).toArray();
 
     return res.status(200).json(files);
   }
